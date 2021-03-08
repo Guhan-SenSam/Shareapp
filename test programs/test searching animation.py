11004cda8af7786b6653754d914af31ee46393c1
Guhan-SenSam <guhansensam@gmail.com>
2021-03-08 22:31:42 +0530

    Updated with proper architecture

--- test programs/test searching animation.py
+++ test programs/test searching animation.py
@@ -3,24 +3,29 @@ from kivy.lang import Builder
 from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Line)
 from kivy.graphics.context_instructions import Color
 from kivy.animation import Animation
-
+from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
+
+from functools import partial
 
 class Test_Loader(FloatLayout):
 
     def __init__(self, **kwargs):
         super(Test_Loader, self).__init__(**kwargs)
         self.starting_angle = 0
-        self.ending_angle = 360
+        self.ending_angle = 0
 
 
         with self.canvas:
             Color(0,1,1,1)
-            Ellipse(angle_start = self.starting_angle,
-                    angle_end = self.ending_angle)
-        for a in range(360):
-            self.starting_angle = a
-            print(self.starting)
+            Ellipse(angle_start = 0,
+                    angle_end = 0)
+        Clock.schedule_once(partial(Test_Loader.animator, self))
+
+
+    def animator(self, *args):
+        anim1 = Animation(ending_angle = 360, duration = 2)
+        anim1.start(self)
 
 class CircleApp(MDApp):
     def build(self):
